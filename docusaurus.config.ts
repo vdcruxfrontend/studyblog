a713527fba37ae1344f41ff52169c81b25f12747
A <movie3342@gmail.com>
2024-01-06 12:59:49 +0900

    Fix: docusaurus.config.ts and deploy.yml file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,18 +1,20 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
-
+const organizationName = "vdcruxfrontend";
+const projectName = "studyblog";
 const config: Config = {
   title: "브이디크럭스 프론트팀 스터디 블로그",
   tagline: "브이디크럭스 프론트 스터디 블로그입니다.",
   favicon: "img/favicon.ico",
-  url: "https://vdcruxfrontend.github.io",
-  baseUrl: "/studyblog/",
+  url: `https://${organizationName}.github.io`,
+  baseUrl: `/${projectName}/`,
   deploymentBranch: "gh-pages",
-  organizationName: "vdcruxfrontend",
-  projectName: "studyblog",
+  organizationName,
+  projectName,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
+  trailingSlash: false,
   i18n: {
     defaultLocale: "en",
     locales: ["en"]
@@ -23,13 +25,11 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`
         },
         theme: {
           customCss: "./src/css/custom.css"
@@ -55,56 +55,14 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: `https://github.com/${organizationName}/${projectName}/`,
           label: "GitHub",
           position: "right"
         }
       ]
     },
     footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro"
-            }
-          ]
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus"
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus"
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus"
-            }
-          ]
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "/blog"
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus"
-            }
-          ]
-        }
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+      copyright: `Copyright © ${new Date().getFullYear()} 브이디크럭스 프론트엔드 팀`
     },
     prism: {
       theme: prismThemes.github,
